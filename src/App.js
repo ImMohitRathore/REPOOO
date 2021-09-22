24195b5aa69908d5497c0b63f84b713c9eff5374
mohit rathore <hackside715@gmail.com>
2021-09-22 14:59:42 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DataListPages from "./component/DataListPages"
+import Match from "./component/Match"
+
+const App = () => {
+  return (
+    <>
+      
+      <Match/>
+    </>
+
+  )
+}
+
+export default App
